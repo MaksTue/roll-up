14c8adb4b99dd6ee083ace27dd627ea10db270bd
MaksTue <pro100max48@gmail.com>
2024-04-14 02:33:12 +0300

    add mobile adapt

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var overlay = document.getElementById("overlay");
 var popupWindow = document.getElementById("popup-window-basket");
 var popupLink = document.getElementById("popup-link-basket");
+var popupLinkMobile = document.getElementById("popup-link-basket-mobile");
 var closeButton = document.getElementById("close-button");
 
 popupLink.addEventListener("click", function (event) {
@@ -10,7 +11,25 @@ popupLink.addEventListener("click", function (event) {
   overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
 });
 
+popupLinkMobile.addEventListener("click", function (event) {
+  event.preventDefault();
+  popupWindow.style.display = "block";
+  overlay.style.display = "block";
+  overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+});
+
 closeButton.addEventListener("click", function () {
   popupWindow.style.display = "none";
   overlay.style.display = "none";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mobileNavToggle = document.querySelector(".mobile-nav-toggle");
+  var navbar = document.querySelector(".mobile-list-header");
+
+  mobileNavToggle.addEventListener("click", function () {
+    this.classList.toggle("visible");
+
+    navbar.classList.toggle("visible");
+  });
 });
